6963169ede9b197022358f250482a5ecdf743767
Breno Guimarães <brenoaraujo@outlook.com.br>
2022-03-15 16:11:35 -0300

    chore: remove headless option

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const puppeteer = require('puppeteer');
 
 async function init() {
-    const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
     await page.goto(process.env.KAIROS_URL);
